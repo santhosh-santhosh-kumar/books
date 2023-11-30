02db7aa6337e48e6bdf0d474381d2746e89cfa86
santhosh-santhosh-kumar <santhoshselvakumar51@gmail.com>
2023-11-30 11:48:43 +0530

    clear all errors

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,7 +5,6 @@ const Topbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-primary sticky-top py-3 mb-2">
         <div className="container px-4 px-lg-5">
-        <i className="fa-solid fa-globe fa-2xl text-white mr-2"></i>
           <Link className="navbar-brand text-light" >
             Library
           </Link>
@@ -24,19 +23,16 @@ const Topbar = () => {
             <ul className="navbar-nav  me-auto mb-2 mb-lg-0 ms-lg-4">
               <li className="nav-item ">
               <Link className="nav-link text-light" to={'/'}>
-            <i className="fas fa-book-open mx-1"></i>
             <span>Books</span>
         </Link>
               </li>
               <li className="nav-item">
         <Link className="nav-link text-light" to={'/addbook'}>
-            <i className="fas fa-plus mx-1"></i>
             <span>Add Books</span>
         </Link>
     </li>
     <li className="nav-item">
         <Link className="nav-link text-light" to={'/allbooks'}>
-            <i className="fas fa-list mx-1"></i>
             <span>View All Books</span>
         </Link>
     </li>
